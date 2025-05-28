5f775e5dc36815d25d5cce4e991aa6f72fdb83b0
Digidon727 <ivntik77@gmail.com>
2025-05-29 00:24:11 +0600

    commit 17

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    public function index(){
+    public function index()
+    {
+        $productsCount = Product::count();
+        $ordersCount = Order::count();
+        $usersCount = User::count();
 
-        return view('admin.index');
+        return view('admin.index', [
+            'productsCount' => $productsCount,
+            'ordersCount' => $ordersCount,
+            'usersCount' => $usersCount,
+        ]);
     }
 }
--- resources/views/admin/layouts/default.blade.php
+++ resources/views/admin/layouts/default.blade.php
@@ -38,16 +38,27 @@
             <ul class="navbar-nav">
                 <li class="nav-item"><a class="nav-link" data-lte-toggle="sidebar" href="#" role="button"> <i
                             class="bi bi-list"></i> </a></li>
-                <li class="nav-item d-none d-md-block"><a href="{{route('admin.main.index')}}" class="nav-link">Главная</a></li>
-                <li class="nav-item d-none d-md-block"><a href="{{route('admin.category.index')}}" class="nav-link">Категории</a></li>
-                <li class="nav-item d-none d-md-block"><a href="" class="nav-link">Склад</a></li>
+                <li class="nav-item d-none d-md-block"><a href="{{route('admin.main.index')}}"
+                                                          class="nav-link">Главная</a></li>
+                <li class="nav-item d-none d-md-block"><a href="{{route('admin.users.index')}}"
+                                                          class="nav-link">Пользователи</a></li>
+                <li class="nav-item d-none d-md-block"><a href="{{route('admin.category.index')}}" class="nav-link">Категории</a>
+                </li>
+                <li class="nav-item d-none d-md-block"><a href="{{route('admin.products.index')}}" class="nav-link">Склад</a></li>
+                <li class="nav-item d-none d-md-block"><a href="{{route('admin.orders.index')}}" class="nav-link">Заказы</a></li>
             </ul> <!--end::Start Navbar Links--> <!--begin::End Navbar Links-->
             <ul class="navbar-nav ms-auto"> <!--begin::Navbar Search-->
-                <li class="nav-item"><a class="nav-link" data-widget="navbar-search" href="#" role="button"> <i
-                            class="bi bi-search"></i> </a></li> <!--end::Navbar Search-->
+                <li class="nav-item d-none">
+                    <a class="nav-link" data-widget="navbar-search" href="#" role="button"> <i
+                            class="bi bi-search"></i>
+                    </a>
+                </li> <!--end::Navbar Search-->
                 <!--begin::Messages Dropdown Menu-->
-                <li class="nav-item dropdown"><a class="nav-link" data-bs-toggle="dropdown" href="#"> <i
-                            class="bi bi-chat-text"></i> <span class="navbar-badge badge text-bg-danger">3</span> </a>
+                <li class="nav-item dropdown d-none">
+                    <a class="nav-link" data-bs-toggle="dropdown" href="#">
+                        <i
+                            class="bi bi-chat-text"></i>
+                        <span class="navbar-badge badge text-bg-danger">3</span> </a>
                     <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end"><a href="#" class="dropdown-item">
                             <!--begin::Message-->
                             <div class="d-flex">
@@ -108,7 +119,7 @@
                         <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
                     </div>
                 </li>
-                <li class="nav-item dropdown">
+                <li class="nav-item dropdown d-none">
                     <a class="nav-link" data-bs-toggle="dropdown" href="#">
                         <i
                             class="bi bi-bell-fill"></i> <span class="navbar-badge badge text-bg-warning">15</span> </a>
@@ -129,38 +140,33 @@
                         </a>
                     </div>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item d-none">
                     <a class="nav-link" href="#" data-lte-toggle="fullscreen">
                         <i
                             data-lte-icon="maximize" class="bi bi-arrows-fullscreen">
 
                         </i> <i data-lte-icon="minimize"
-                                                                                             class="bi bi-fullscreen-exit"
-                                                                                             style="display: none;"></i>
+                                class="bi bi-fullscreen-exit"
+                                style="display: none;"></i>
                     </a></li>
-                <li class="nav-item dropdown user-menu"><a href="#" class="nav-link dropdown-toggle"
-                                                           data-bs-toggle="dropdown"> <img
-                            src="{{asset('assets/admin/assets/img/user2-160x160.jpg')}}"
+                <li class="nav-item dropdown user-menu">
+                    <a href="#" class="nav-link dropdown-toggle"
+                       data-bs-toggle="dropdown"> <img
+                            src="https://electro/assets/admin/assets/img/AdminLTELogo.png"
                             class="user-image rounded-circle shadow"
-                            alt="User Image"> <span class="d-none d-md-inline">Alexander Pierce</span> </a>
+                            alt="User Image"> <span class="d-none d-md-inline">{{auth()->user()->email}}</span> </a>
                     <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-end"> <!--begin::User Image-->
                         <li class="user-header text-bg-primary"><img
                                 src="{{asset('assets/admin/assets/img/user2-160x160.jpg')}}"
                                 class="rounded-circle shadow" alt="User Image">
                             <p>
-                                Alexander Pierce - Web Developer
-                                <small>Member since Nov. 2023</small>
+                                {{auth()->user()->email}}
+                                <small class="d-none">Member since Nov. 2023</small>
                             </p>
-                        </li> <!--end::User Image--> <!--begin::Menu Body-->
-                        <li class="user-body"> <!--begin::Row-->
-                            <div class="row">
-                                <div class="col-4 text-center"><a href="#">Followers</a></div>
-                                <div class="col-4 text-center"><a href="#">Sales</a></div>
-                                <div class="col-4 text-center"><a href="#">Friends</a></div>
-                            </div> <!--end::Row-->
-                        </li> <!--end::Menu Body--> <!--begin::Menu Footer-->
-                        <li class="user-footer"><a href="#" class="btn btn-default btn-flat">Profile</a> <a href="#"
-                                                                                                            class="btn btn-default btn-flat float-end">Sign
+                        </li>
+                        <li class="user-footer"><a href="{{route('account.form')}}" class="btn btn-default btn-flat">Аккаунт</a>
+                            <a href="{{route('logout')}}"
+                               class="btn btn-default btn-flat float-end">Выйти
                                 out</a></li> <!--end::Menu Footer-->
                     </ul>
                 </li> <!--end::User Menu Dropdown-->
@@ -171,11 +177,11 @@
     <aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark"> <!--begin::Sidebar Brand-->
         <div class="sidebar-brand">
             <a href="{{route('admin.main.index')}}" class="brand-link">
-                 <img src="{{asset('assets/admin/assets/img/AdminLTELogo.png')}}"
-                                               alt="AdminLTE Logo"
-                                               class="brand-image opacity-75 shadow"> <!--end::Brand Image-->
-               <span class="brand-text fw-light">AdminLTE 4</span> <!--end::Brand Text--> </a>
-             </div>
+                <img src="{{asset('assets/admin/assets/img/AdminLTELogo.png')}}"
+                     alt="AdminLTE Logo"
+                     class="brand-image opacity-75 shadow"> <!--end::Brand Image-->
+                <span class="brand-text fw-light">AdminLTE 4</span> <!--end::Brand Text--> </a>
+        </div>
         <div class="sidebar-wrapper">
             <nav class="mt-2"> <!--begin::Sidebar Menu-->
                 <ul class="nav sidebar-menu flex-column" data-lte-toggle="treeview" role="menu" data-accordion="false">
@@ -296,7 +302,7 @@
 <!-- jsvectormap -->
 
 <script src="{{asset('assets/admin/js/adminlte.js')}}"></script>
-<script src="{{ asset('assets/admin/ckeditor/ckeditor.js') }}" ></script>
+<script src="{{ asset('assets/admin/ckeditor/ckeditor.js') }}"></script>
 <script src="{{asset('assets/admin/js/main.js')}}"></script>
 
 
