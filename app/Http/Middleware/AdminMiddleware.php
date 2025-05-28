d0c18467baec81fa69d808b800130c6f3770f6e7
Digidon727 <ivntik77@gmail.com>
2025-05-29 01:42:04 +0600

    commit 20

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // Предположим, у пользователя есть поле is_admin
+        if (Auth::check() && Auth::user()->role === 3) {
+            return $next($request);
+        }
+
+        abort(403, 'Доступ запрещён');
+    }
+}
